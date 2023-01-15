5422b7d7a147645e94615b4f54b9e295d418bdf3
marcio carolino <marciocarolino@gmail.com>
2023-01-15 19:58:22 -0300

    start of database setup

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            entities: [],
-        }),
-    ],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: 5432,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      entities: [],
+      migrations: [],
+      synchronize: false,
+    }),
+  ],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
